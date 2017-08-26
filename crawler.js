4cffa074b17c0465de0f50069ac69d485f6bc6ca
Mark Halls <firesetterdev@gmail.com>
2017-08-25 18:26:00 -0700

    need to fix promise return

--- crawler.js
+++ crawler.js
@@ -1,9 +1,7 @@
-const axios = require('axios');
-const jsdom = require('jsdom');
-const URL = require('url').Url;
+const axios = require("axios");
+const { JSDOM } = require("jsdom");
+const { URL } = require("url");
 
-const { JSDOM } = jsdom;
-// const { URL } = url;
 const pageToVisit = "http://www.schlockmercenary.com";
 const nextPageSelector = "";
 const cssQuery = ".strip-image-wrapper";
@@ -11,60 +9,62 @@ const nextPage = "";
 
 console.log("visiting page: " + pageToVisit);
 
+const getPage = pageToVisit => {
+  let imgSrc = "";
+  axios
+    .get(pageToVisit)
+    .then(response => {
+      console.log("Status Code: " + response.status);
+      if (response.status === 200) {
+        const data = response.data;
+        const dom = new JSDOM(data);
+        imgSrc = dom.window.document
+          .querySelector(cssQuery)
+          .querySelector("img").src;
+        console.log(imgSrc);
+      }
+    })
+    .catch(error => console.error(error));
+  console.log("test" + imgSrc);
+  return imgSrc;
+};
 
-axios.get(pageToVisit)
-	.then(response => {
-		console.log("Status Code: " + response.status);
-		if(response.status === 200) {
-			// console.log(response);
-			const data = response.data;
-			const dom = new JSDOM(data);
-			const imgSrc = dom.window.document.querySelector(
-				cssQuery).querySelector("img").src;
-			const srcFileName = new URL(imgSrc);//.pathname.split("/").pop();
-			// const srcFileName = imgSrc.split('/').pop().split('?').shift();	
-			// const srcFileName = imgSrc.replace(/(.+)\w+.[jpg|png|gif](.+)/g, "");
-			
-			console.log(srcFileName);
+const getImage = relativeURL => {
+  const srcFileName = new URL(relativeURL, pageToVisit).pathname
+    .split("/")
+    .pop();
+  console.log(relativeURL);
+  axios({
+    method: "get",
+    url: relativeURL,
+    responseType: "stream"
+  })
+    .then(response => {
+      response.data.pipe(fs.createWriteStream(srcFileName));
+    })
+    .catch(err => console.error(err));
+};
 
-			console.log(dom.window.document.querySelector(
-				".strip-image-wrapper").querySelector("img").src);
-			// return { imgSrc, srcFileName }		
-		}
-	})
-	.catch(error => console.error(error));
-	
-const getImage = (URL, srcFileName) => {
-	axios({
-		method: 'get',
-		url: URL,
-		responseType: 'stream'
-	})
-	  .then(response => {
-			response.data.pipe(fs.createWriteStream(srcFileName))
-		});
-}	
 const searchForWord = ($, word) => {
-	const bodyText = $('html > body').text();
-	if(bodyText.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
-		return true;
-	}
-	return false;
-}
+  const bodyText = $("html > body").text();
+  if (bodyText.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
+    return true;
+  }
+  return false;
+};
 
-const collectInternalLinks = ($) => {
-	let allRelativeLinks = [],
-			allAbsoluteLinks = [];
-	
-	const relativeLinks = $("a[href^='/']");
-	relativeLinks.each(() => allRelativeLinks.push($(this)
-	  .attr('href')));
+const collectInternalLinks = $ => {
+  let allRelativeLinks = [],
+    allAbsoluteLinks = [];
 
-	const absoluteLinks = $("a[href^='http']");
-	absoluteLinks.each(() => allAbsoluteLinks.push($(this).attr('href')));
+  const relativeLinks = $("a[href^='/']");
+  relativeLinks.each(() => allRelativeLinks.push($(this).attr("href")));
 
-	console.log("Found " + allRelativeLinks.length + " relative links");
-	console.log("Found " + allAbsoluteLinks.length + " absolute links");
-}
+  const absoluteLinks = $("a[href^='http']");
+  absoluteLinks.each(() => allAbsoluteLinks.push($(this).attr("href")));
 
+  console.log("Found " + allRelativeLinks.length + " relative links");
+  console.log("Found " + allAbsoluteLinks.length + " absolute links");
+};
 
+getPage(pageToVisit);
